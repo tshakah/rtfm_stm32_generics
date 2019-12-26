a6a5f25f131fbb9279f960dd898ee32a490168bf
Elisha Hastings <5149755+tshakah@users.noreply.github.com>
2019-12-26 19:31:46 +0000

    Update nix config

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@ stdenv.mkDerivation rec {
   name = "env";
   env = buildEnv { name = name; paths = buildInputs; };
   buildInputs = [
-    binutils gcc gnumake openssl pkgconfig
+    binutils gcc gnumake openssl pkgconfig gdb-multitarget openocd gcc-arm-embedded libusb
     nodejs
   ];
 }
